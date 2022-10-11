5a6590e2fe84ce64e24499e77d33bffa9b51f3ef
elfenbeinstein <96117493+elfenbeinstein@users.noreply.github.com>
2022-10-11 17:06:46 +0200

    cam behaviour update -- not fully functional

---
+++ Assets/Scripts_Tabea/CameraBehaviour.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject player; // find via script later
+    private Transform camPos;
+
+    private float mouseX, mouseY;
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private float maxRotation;
+    [SerializeField] private float minRotation;
+    private float verticalRotation;
+
+    void Start()
+    {
+        camPos = player.GetComponent<Movement>().CamPos();
+        //gameObject.transform.position = camPos.position;
+        verticalRotation = 0;
+    }
+
+    void Update()
+    {
+        /*
+        // calculate rotation:
+        mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, minRotation, maxRotation);
+
+        // rotate with mouse:
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        //player.transform.Rotate(Vector3.up, mouseX);
+        */
+        //gameObject.transform.position = camPos.position;
+    }
+}
