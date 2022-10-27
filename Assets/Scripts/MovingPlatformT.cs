199a009061aad18bc40724b3d68a1d9094deda07
elfenbeinstein <96117493+elfenbeinstein@users.noreply.github.com>
2022-10-27 19:59:49 +0200

    moving platform added

---
+++ Assets/Scripts/MovingPlatformT.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformT : MonoBehaviour
+{
+    [SerializeField] private Transform pos1;
+    [SerializeField] private Transform pos2;
+    [SerializeField] private Vector3 direction;
+    bool up;
+
+    void Start()
+    {
+        gameObject.transform.position = pos1.position;
+        up = true;
+    }
+
+    void Update()
+    {
+        if (up)
+        {
+            gameObject.transform.position += direction / 1000;
+            if (Vector3.Distance(gameObject.transform.position, pos2.position) <= 0.3f)
+            {
+                up = false;
+            }
+        }
+        else
+        {
+            gameObject.transform.position -= direction / 1000;
+            if (Vector3.Distance(gameObject.transform.position, pos1.position) <= 0.3f)
+            {
+                up = true;
+            }
+        }
+    }
+}
