225f6b8f5d0fef3e18dee5d2549b4ffe66fde548
elfenbeinstein <96117493+elfenbeinstein@users.noreply.github.com>
2022-11-09 17:44:00 +0100

    player audio script 1

---
+++ Assets/Scripts/Player Controller/PlayerAudio.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip jump;
+    [SerializeField] [Range(0, 1 )] private float jumpVolume;
+
+    [SerializeField] private AudioClip dash;
+    [SerializeField] [Range(0, 1)] private float dashVolume;
+
+    [SerializeField] private AudioClip shoot;
+    [SerializeField] [Range(0, 1)] private float shootVolume;
+
+    [SerializeField] private AudioClip death;
+    [SerializeField] [Range(0, 1)] private float deathVolume;
+
+    [SerializeField] private AudioClip goal;
+    [SerializeField] [Range(0, 1)] private float goalVolume;
+
+    [SerializeField] private AudioClip start;
+    [SerializeField] [Range(0, 1)] private float startVolume;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        EventSystem.Instance.AddEventListener("AUDIO", AudioListener);
+    }
+
+    private void OnDestroy()
+    {
+        EventSystem.Instance.RemoveEventListener("AUDIO", AudioListener);
+    }
+
+    private void AudioListener(string eventName, object param)
+    {
+        if (eventName == "jump") PlayJump();
+        if (eventName == "dash") PlayDash();
+        if (eventName == "shoot") PlayShoot();
+        if (eventName == "death") PlayDeath();
+        if (eventName == "start") PlayStart();
+        if (eventName == "goal") PlayGoal();
+    }
+
+    [ContextMenu("jump")]
+    private void PlayJump()
+    {
+        audioSource.PlayOneShot(jump, jumpVolume);
+    }
+
+    [ContextMenu("dash")]
+    private void PlayDash()
+    {
+        audioSource.PlayOneShot(dash, dashVolume);
+    }
+
+    [ContextMenu("shoot")]
+    private void PlayShoot()
+    {
+        audioSource.PlayOneShot(shoot, shootVolume);
+    }
+
+    [ContextMenu("death")]
+    private void PlayDeath()
+    {
+        audioSource.PlayOneShot(death, deathVolume);
+    }
+
+    [ContextMenu("goal")]
+    private void PlayGoal()
+    {
+        audioSource.PlayOneShot(goal, goalVolume);
+    }
+
+    [ContextMenu("start")]
+    private void PlayStart()
+    {
+        audioSource.PlayOneShot(start, startVolume);
+    }
+}
