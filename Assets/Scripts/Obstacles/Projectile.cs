c0c0d172649ce85f264ba950b19494c4056cf549
JxlianR <j.reiter022002@gmail.com>
2022-11-16 16:30:12 +0100

    Comments on Julian's Scripts

--- Assets/Scripts/Obstacles/Projectile.cs
+++ Assets/Scripts/Obstacles/Projectile.cs
@@ -5,35 +5,35 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed, duration, force, pushDuration;
-    private CommandMovement movePlayer;
-    public Actor actor;
 
     private void Start()
     {
+        //Destroy projectile after a set duration
         Destroy(gameObject, duration);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Moves the projectile forward
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Check if other object is a player
         if (collision.gameObject.GetComponent<InputHandlerAlive>() != null)
         {
             //Push Player back
-
-            actor = collision.gameObject.GetComponent<Actor>();
-            Vector3 pushDirection = collision.transform.position - transform.position;
+            Vector3 pushDirection = collision.transform.position - transform.position; // Vector for the direction the player gets pushed
             pushDirection.Normalize();
 
-            collision.gameObject.GetComponent<InputHandlerAlive>().Push(pushDirection * force * Time.deltaTime, pushDuration);
+            collision.gameObject.GetComponent<InputHandlerAlive>().Push(pushDirection * force * Time.deltaTime, pushDuration); // Calls function in the InputHandlerAlive script that pushes player back
 
-            EventSystem.Instance.Fire("AUDIO", "ouch");
+            EventSystem.Instance.Fire("AUDIO", "ouch"); // Plays audio
         }
         
+        //Destroy object when it collides with another object
         if (collision.gameObject.GetComponent<InputHandlerDead>() == null)
             Destroy(gameObject);
     }
--- Assets/Scripts/Player Controller/CheckPoint.cs
+++ Assets/Scripts/Player Controller/CheckPoint.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    // Script for every checkpoint that contains the according spawnpoint
     public GameObject spawnpoint;
 
     private void OnTriggerEnter(Collider other)
     {
+        // Sets the current spawnpoint for the player to the spawnpoint of this checkpoint
         if (other.gameObject.GetComponent<InputHandlerAlive>() != null)
         {
             other.gameObject.GetComponent<InputHandlerAlive>().currentSpawnpoint = spawnpoint;
